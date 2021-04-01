3cfdf10ba043229d9805d2b3c7122b201a95baef
Eugene Kusey <evgen.kusey@gmail.com>
2021-04-01 11:36:11 +0300

    Создал переменные, console log, alert

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-alert('hello');
-var myVar2 = 10;
+var money = 18;
+let income = 30000;
+let addExpenses = 5000;
+let deposit = 100000;
+let mission = 10000;
+let period = 5;
+
+alert(income);
+console.log(money);
